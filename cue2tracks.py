566253ec1bf43679897891d5b2a9578bafb702c0
mistekko <steamlie@protonmail.com>
2024-02-17 09:20:38 -0600

    made the code actually function

--- cue2tracks.py
+++ cue2tracks.py
@@ -3,21 +3,21 @@ import sys, os
 
           #this program assumes CUE files provided as input will be (computer generated) of standard structure:
           #- INDEX 01 is the last track field
-          #- the track metadata is the last set of fields
+          #- the tracks metadata is the last set of fields
           #it also assumes that the CUE only references one audio file. 
           
 metadata = [] #this is all the album metadata
 audio_file = [] #rather than have the user supply the CUE's corresponding audio file, let's detect it outomatically
 
 
-track_list = [] #contains dictionaries
+track_list = [] #will contain dictionaries
 
 
 #track metadata
 track = ""
 title = ""
 isrc = ""
-performer = "" #this is what CUE calls the artist! Why!
+performer = "" #this is what CUE calls the artist! 
 index00 = "" #timestamp of previos track's end in MM:SS:mS... convert this later becaus ffmpeg uses HH:MM:SS. May not be used
 index01 = "" #timestamp of this track's beginning.
 
@@ -29,48 +29,49 @@ with open(sys.argv[1], 'r') as cue:
         metadata.append(line.strip()) #will probably change this in the future; right now, it's just there becuase I want to store this data, but I'm not sure how it's going to be used exactly yet
         line = cue.readline()
 
-    audio_file = line[6:-7]
-    print(f"Converting file: {audio_file:>60}")
+    audio_file = line[6:-7] #the last 7 characters are " WAVE, which we don't care about. The first 6, FILE ", are similarly irrelevant
+    print(f"Converting file:{audio_file:>60}") 
           
-    #the above works properly; trust me
+    #the above works properly; trust me; trust yourself
 
     #grab the file extension for later use
     for n in range(1,len(audio_file)):
         if audio_file[-n] == ".":
             file_extension = audio_file[-n:]
-            print("Detected file extension:\t" + file_extension)
-            n = len(audio_file)
-            break
+            print(f"Detected file extension:{file_extension:>52}")
 
-
-    while True:
-        track = cue.readline().strip().split(' ')[1]
-        print("track: " + track)
-        line = cue.readline()
+    track = cue.readline()
+    while track != "":
+        track = track.strip().split(' ')[1] #grabs the second word in the line after FILE. This should be something like "TRACK 01 AUDIO", meaning the track number should be the second word
+        print("track: " + track) #works up to this point
         while line != "":
-            if line.find("TITLE") > -1:
-                title = line.strip("TITLE ").strip("\"")
-            elif line.find("PERFORMER") > -1:
-                title = line.split("PERFORMER ")[1].strip('\"')[0] #assumes the artist's name does not start with a space
+            line = cue.readline()
+            if line.find("PERFORMER") > -1:
+                performer = line.split("PERFORMER ")[1].strip('\"')[0:-2] #assumes the artist's name does not start with a space
+                print(f"\tparsed performer {performer}")
+            elif line.find("TITLE") > -1:
+                title = line.strip().strip("TITLE ").strip("\"")
+                print(f"\tparsed title {title}")
             elif line.find("ISRC") > -1:
                 isrc = line.split("ISRC ")[1].strip()
+                print(f"\tparsed irsc {isrc}")
             elif line.find("INDEX 00") > -1:
                 index00 = line.split("INDEX 00 ")[1].strip()
+                print(f"\tparsed index00 {index00}")
             elif line.find("INDEX 01") > -1:
                 index01 = line.split("INDEX 01 ")[1].strip()
                 #construct a dictionary of the just-parsed tracked and append it to track_list
+                print(f"\tparsed index01 {index01}")
                 track_list.append({"track" : track,
                                     "title" : title,
                                     "performer" : performer,
                                     "isrc" : isrc,
                                     "index00" : index00,
                                     "index01" : index01})
-                print(track_list)
                 print()
-                
+                break
 
-            
-
+        track = cue.readline() #this line is down here so the value in 'track' can be used to detect the end of the file and end iteration accordingly. This requires it to get its value BEFORE each iteration in which said value is used. Either that or check it each iteration, which is an extra conditional every iteration. Not the end of the world, but it is bloat that doesn't geatly improve the readability or intelligibility of my code (or so I think...)
             
     print(metadata, file_extension)         
     
