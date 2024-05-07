79e2997c7a2f4151d952aa5bf4e0dc219f2b20db
mistekko <steamlie@protonmail.com>
2024-05-07 10:32:56 -0500

    fixed types for timestamp-second conversion

--- c2t.py
+++ c2t.py
@@ -1,19 +1,36 @@
 #! /usr/bin/env python
 import sys, os
 from os.path import splitext
+from decimal import * 
 
+
+"""
+Helper functions below
+"""
 def fix_time(input_time):
     """Converts a time from CUE format (MM:SS:cS) to ffmpeg format (HH:MM:SS.cS)
-
-    Args:
-        input_time (str): time to be converted
-
-    Returns:
-    	string: converted time
     """
 
     return "00:"+input_time[0:-3]+"."+input_time[-2:]
 
+
+def convert_to_seconds(time):
+    """Converts a colon-separated time in ffmpeg format into the number of seconds from epoch (i.e., 00:00:00)
+    """
+    
+    times = time.split(':') #times is a list containing HH, MM, SS.mS... extracted from time
+    print(times)
+    uh = int(int(times[0])*3600 + int(times[1]) * 60 + Decimal(times[2]))
+    return uh
+
+def subtract_times(time_one, time_two):
+    print(time_one, time_two)
+    return abs(int(convert_to_seconds(time_one)) - int(convert_to_seconds(time_two)))
+
+
+"""
+code of substance below
+"""
 def parse_cue_file(cue_path):
     """Parses a CUE file and returns a dictionary containing metadata
     and a list of track dictionaries.
@@ -85,20 +102,22 @@ def convert_tracks(metadata, track_list, audio_file):
     """
 
 
-    for a in range(0,len(track_list)): #the last track won't need a different command since it won't have a track after it from which the track's end index can be extracted
+    for a in range(0,len(track_list)): #the last track will  need a different command since it won't have a track after it from which the track's end index can be extracted
         current_track = track_list[a]
         file_extension = metadata[len(metadata)-1]
         current_track_file_name = current_track["track"] + ". " + current_track["title"] + "." + file_extension
         current_track_index01 = fix_time(current_track['index01'])
-        next_track_index01 = track_list[a+1]['index01']
         
         
         if a == len(track_list)-1:
-            ffmpeg_command = f"ffmpeg -i \"{audio_file}\" -ss {current_track_index01} -c:a copy \"{current_track_file_name}\" -y"
+            ffmpeg_command = f"ffmpeg -ss {current_track_index01} -i \"{audio_file}\" -map_metadata -1 -c copy \"{current_track_file_name}\" -y"
             break
         else:
-            ffmpeg_command = f"ffmpeg -i \"{audio_file}\" -ss {current_track_index01} -to {next_track_index01} -c:a copy \"{current_track_file_name}\" -y"
-        print(ffmpeg_command) # debug line
+            next_track_index01 = fix_time(track_list[a+1]['index01'])
+            print(current_track_index01, next_track_index01)
+            duration = subtract_times(current_track_index01, next_track_index01)
+            ffmpeg_command = f"ffmpeg -ss {current_track_index01} -i \"{audio_file}\" -t {next_track_index01} -map_metadata -1 -c copy \"{current_track_file_name}\" -y"
+        print(ffmpeg_command) # debug line; remove in release version
             
         os.system(ffmpeg_command)
     
