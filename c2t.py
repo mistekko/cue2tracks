97d335f45246e697b12bbac0f817e8c4a1728eb3
mistekko <steamlie@protonmail.com>
2024-07-03 01:18:20 -0500

    Amended style across entire file and reached 200 lines of code

--- c2t.py
+++ c2t.py
@@ -1,44 +1,46 @@
 #! /usr/bin/env python
-import sys, os
+import sys
+import os
 from os.path import splitext
 from decimal import *
 from pprint import pp
 
 
-"""
-Helper functions 
-"""
+
+"""Helper functions """
 def convert_to_seconds(time):
-    """Converts a colon-separated CUE-style time (MM:SS:cS) into the the fully numerical unit of seconds
     """
-    
-    times = time.split(':') #times is a list containing HH, MM, SS.mS... extracted from time
+    Converts a colon-separated CUE-style time (MM:SS:cS) into the the
+    fully numerical unit of seconds
+    """
+
+    # times is a list containing HH, MM, SS.mS... extracted from time
+    times = time.split(':') 
     seconds = int(int(times[0])*60 + int(times[1]) + Decimal(times[2])/100)
     return seconds
 
 def subtract_times(time_one, time_two):
 
-    """subtracts two times passed as CUE-style timestamps and returns their difference in seconds
-    """
+    """subtracts two times passed as CUE-style timestamps and returns their difference in seconds"""
     
-    return abs(int(convert_to_seconds(time_one)) - int(convert_to_seconds(time_two)))
+    return (abs(int(convert_to_seconds(time_one))
+            - int(convert_to_seconds(time_two))))
 
 
 
-"""
-Main class containing methods for cue-2-track-ing
-"""
+"""Main class containing methods for cue-2-track-ing"""
 class cue2tracks:
 
     def __init__(self):
         print("object created")
 
-
+        
+    """
+    Methods used for parsing and conversion are found below. These are
+    all holdevers from before they were methods of a class, but I've
+    kept them since the devision seems logical to me
+    """
     
-    """
-    methods needed to not have to rewrite the entire script from scratch:
-    """
-
     def parse_cue_file(self, cue_path):
         """
         Parses a CUE file and returns a dictionary containing metadata
@@ -48,8 +50,8 @@ class cue2tracks:
             cue_path (str): Path to the CUE file.
 
         Returns:
-            dict: Dictionary containing album metadata.
-            list: List of track dictionaries, each containing track information.
+            dict: Dictionary containing *album* metadata.
+            list: List of dictionaries containing *track* information.
         """
 
         print(f"parsing file: {cue_path}")
@@ -67,19 +69,18 @@ class cue2tracks:
                 if line.startswith("REM"):
                     key = line.split()[1]
                     metadata[key] = line.removeprefix(f"REM {key} ").strip("\"")
-                    # print(f"\tkey: {key}\n\tvalue: {metadata[key]}")
                     num = 28-len(key)
                     print(f"{key}: {metadata[key]:>{28-len(key)}}")
-                    #second word equals the rest of the list/string
                 else:
                     key = line.split()[0]
                     metadata[key] = line.removeprefix(key + " ").strip("\"")
                     print(f"{key}: {metadata[key]:>{28-len(key)}}")
-                    #first word equals the rest of the list/strong
 
-            # Parse audio file and extension
+                
+            """Parse audio file's name and extension"""            
             audio_file = line.split()
-            audio_file = " ".join(audio_file[1:-1])[1:-1] #gets rid of first and last word and first and last character of middle section
+            # gets rid of first and last word and first and last character of middle section
+            audio_file = " ".join(audio_file[1:-1])[1:-1] 
             file_extension = splitext(audio_file)[1]
             print(f"Detected file extension: {file_extension:>5}")
             metadata["file extension"] = file_extension
@@ -107,42 +108,61 @@ class cue2tracks:
                             "performer": performer,
                             "isrc": isrc,
                             "index00": index00,
-                            "index01": index01,
-                        })
+                            "index01": index01,})
                         break
 
             return metadata, track_list, audio_file
 
-
     def convert_tracks(self, metadata, track_list, audio_file):
         """
-        Uses system's ffmpeg installation in path to convert a flac file into individual tracks according to track_list
+        Uses system's ffmpeg installation in path to convert a flac file
+        into individual tracks, then uses system's id3v2 installation to
+        tag them according to the track_list.
 
         Args:
+            metadat (dict): a dictionary containing metadat about the album
             track_list (dict): a dictionary containing metadata about the tracks
+            audio_file (string): the name of the audio file to be split
         Returns:
             nothing
         """
 
-
-        for a in range(0,len(track_list)): #the last track will  need a different command since it won't have a track after it from which the track's end index can be extracted
+        tags_keys = [['-A','TITLE'],
+                     ['-c','COMMENT'],
+                     ['-g','GENRE'],
+                     ['-y','DATE']]
+        
+        for a in range(0,len(track_list)):
             current_track = track_list[a]
             file_extension = metadata["file extension"]
-            current_track_file_name = current_track["track"] + ". " + current_track["title"] + "." + file_extension
+            current_track_file_name = current_track["track"] + ". "
+                                      + current_track["title"]
+                                      + "." + file_extension
             current_track_index01 = convert_to_seconds(current_track['index01'])
 
+            # the last track needs a separate command since it can't use
+            # the next track's index as reference
             if a == len(track_list)-1:
-                ffmpeg_command = f"ffmpeg -ss {current_track_index01} -i \"{audio_file}\" -map_metadata -1 \"{current_track_file_name}\" -y"
+                ffmpeg_command = f"ffmpeg\
+                                     -ss {current_track_index01}\
+                                     -i \"{audio_file}\" -map_metadata\
+                                     -1 \"{current_track_file_name}\"\
+                                     -y"
             else:
                 next_track_index01 = convert_to_seconds(track_list[a+1]['index01'])
                 duration = current_track_index01 - next_track_index01
-                ffmpeg_command = f"ffmpeg -ss {current_track_index01} -to {next_track_index01} -i \"{audio_file}\" -map_metadata -1 \"{current_track_file_name}\" -y"
+                ffmpeg_command = f"ffmpeg\
+                                     -ss {current_track_index01}\
+                                     -to {next_track_index01}\
+                                     -i \"{audio_file}\"\
+                                     -map_metadata -1\ #get rid of auto-generated ffmpeg metadata
+                                     \"{current_track_file_name}\"\
+                                     -y" 
             print(ffmpeg_command)
             os.system(ffmpeg_command + " 2> /dev/null")
 
             #The following code tags each file right after its been converted
             id3_command = f"id3v2"
-            tags_keys = [['-a','PERFORMER'], ['-A','TITLE'], ['-c','COMMENT'], ['-g','GENRE'], ['-y','DATE']]
 
             for pair in tags_keys:
                 try:
@@ -150,7 +170,10 @@ class cue2tracks:
                 except KeyError:
                     print(f"{pair[1]} not specificed... skipping")
 
-            id3_command += f" -t \"{current_track['title']}\" -T {current_track['track']} \"{current_track_file_name}\""
+            id3_command += f" -t \"{current_track['title']}\" \
+                              -a \"{current_track['performer']}\" \
+                              -T   {current_track['track']} \
+              		         \"{current_track_file_name}\""
             print(id3_command)
             os.system(id3_command)
 
@@ -161,10 +184,13 @@ class cue2tracks:
         """
 
         """
-        in a very-soon-to-be-released update, we'll have a more proper way of determining the passed cue file.
-        Both in this iteration AND in the just mentioned one, we'll determine cue_path outside of the parsing
-        function. It will almost definitely end up being an instance variable since it'll be sorted from the
-        rest of the elements in sys.argv at the same time as instance variable assignment.
+        in a very-soon-to-be-released update, we'll have a more proper
+        way of determining the passed cue file. Both in this iteration
+        AND in the just mentioned one, we'll determine cue_path outside
+        of the parsing function. It will almost definitely end up being
+        an instance variable since it'll be sorted from the rest of the
+        elements in sys.argv at the same time as instance variable
+        assignment.
         """
         cue_path = sys.argv[1]
         
@@ -175,6 +201,7 @@ class cue2tracks:
 
 
 if __name__ == "__main__":
+
     if len(sys.argv) < 2:
         print(f"Usage: {sys.argv[0]} <CUE_file>")
         sys.exit(1)
